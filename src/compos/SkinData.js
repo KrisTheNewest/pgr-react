e5d55742a052aa0feb8a53c7c0c1415519ec40ba
kris <61835777+KrisTheNewest@users.noreply.github.com>
2023-03-12 18:44:23 +0100

    todo refactor

--- src/compos/SkinData.js
+++ src/compos/SkinData.js
@@ -1,14 +1,37 @@
-import React from 'react';
-// import pgrIcon from './icon.webp';
-// import charaImg from "./chara_bianca.png";
+import React, {useState, useLayoutEffect, useEffect} from 'react';
+import { useParams } from 'react-router-dom';
+import classNames from 'classnames';
+
 import '../App.css';
 
 function SkinData(props) {
+	const { price, event, } = props;
+	// const currentData = 0;
+	const [currentData, setData] = useState(0);
+	const {cost} = useParams();
+	let data = event.map((e, index, arr) => {
+		let temp = {};
+		temp.event = e;
+		temp.price = price[index] ?? price[index - 1] ?? price[0];
+		return temp;
+	});
+	useEffect(() => {
+		setData(0);
+	}, [cost])
+	let currentInfo = data[currentData];
+	// console.log(currentInfo);
+	if (!currentInfo) return ("please wait...");
 	return (
 		<div className="skinData">
-			<aside className="eventList" /*TODO: style=""*/>
-				<button /*TODO: style=""*/>1</button>
-				<button /*TODO: style=""*/>2</button>
+			<aside className="eventList">
+				{data.map((dataItem, index) =>
+					<button 
+						className={classNames(`dataBtn `, currentData === index && "selectedData")} 
+						 key={index}
+					>
+						{index}
+					</button>
+				)}
 			</aside>
 			<div className="priceData">
 				<table className="dataTable">
@@ -20,14 +43,14 @@ function SkinData(props) {
 					</thead>
 					<tbody>
 						<tr>
-							<td>123</td>
-							<td>RC</td>
+							<td>{currentInfo.price.value}</td>
+							<td>{currentInfo.price.currency}</td>
 						</tr>
 					</tbody>
 				</table>
 				<article className="comment prettyScroll">
 					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt voluptatibus nemo aspernatur, quia eius dicta dolor similique molestiae doloremque, magnam dolorem sapiente maiores sed eos aliquid, ullam corrupti culpa dolores? Aliquam asperiores labore accusantium quo quasi dolorem possimus deleniti aliquid, nostrum iure blanditiis, rerum quaerat quae maiores earum tempore sed consectetur quibusdam eos similique totam cupiditate necessitatibus fugiat? Praesentium harum cum molestiae, iure facere suscipit et possimus consequatur vel dolorem rem, nihil voluptatem impedit excepturi quibusdam sit sint non labore dolor sed. Quasi vero reprehenderit beatae non? Quis nam minus tenetur nemo perferendis aperiam ad necessitatibus hic illum! Inventore tenetur aspernatur hic culpa odit consequatur est qui tempore! Nulla aspernatur beatae eveniet officiis nesciunt nemo veniam ut incidunt dicta iusto at, deserunt dolores nobis hic itaque autem, tenetur a atque dolor. Dignissimos unde nostrum corporis. Error distinctio tempore sint labore sit eligendi saepe expedita, iure, inventore, odio obcaecati hic nulla nobis modi? Nostrum quos, tempora nobis est cum quibusdam optio sequi eveniet quae molestias possimus porro illum, reprehenderit recusandae obcaecati non unde. Repellendus, minima? Ducimus, magni. Assumenda quod alias beatae repellendus soluta autem inventore officiis aliquam fugit odit velit eius maxime fugiat possimus, eum, eos magni quis dignissimos nam qui?
+						{currentInfo.price.name}?
 					</p>
 				</article>
 			</div>
@@ -44,17 +67,17 @@ function SkinData(props) {
 					</thead>
 					<tbody>
 						<tr>
-							<td>CN</td>
-							<td>09.10.2022</td>
-							<td>09.11.2022</td>
-							<td>-</td>
-							<td>☑️</td>
+							<td>{currentInfo.event.region}</td>
+							<td>{currentInfo.event.start}</td>
+							<td>{currentInfo.event.finish}</td>
+							<td>{currentInfo.event.rerun}</td>
+							<td>{currentInfo.event.disc}</td>
 						</tr>
 					</tbody>
 				</table>
 				<article className="comment prettyScroll">
 					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsam laborum esse similique distinctio sequi nihil natus hic magnam quos, delectus repellat modi eius, sint harum debitis? Consectetur asperiores ducimus tenetur, doloremque excepturi dolore cupiditate tempore sed perferendis quae autem nostrum culpa ea qui atque possimus quasi sapiente vitae debitis animi quo sequi repudiandae accusamus minus! Odit veniam maiores asperiores quis quaerat. Assumenda possimus recusandae doloribus, eum vel minima officiis rem dolorem delectus. Est dolor laboriosam sit maiores culpa nulla corporis sunt, neque magni nobis similique quaerat voluptates tenetur dicta asperiores velit fugit mollitia explicabo labore autem atque at. Eius fugit vitae repellat, autem cumque reprehenderit laudantium quidem accusamus. Minus quasi, nobis ea ut tempore eveniet. Minus, explicabo voluptas quod, repellendus quasi provident enim quisquam repudiandae nam qui impedit architecto. Deleniti aliquid nihil consectetur cupiditate ea ad, beatae corrupti ex explicabo? Voluptatum aperiam cupiditate doloribus fugit id ipsam ipsa deserunt velit quia earum nobis corrupti perferendis ab animi, molestias porro! Pariatur ipsum eos, magnam tempore, dicta quo voluptatibus accusantium maxime vitae obcaecati accusamus sunt repellendus neque. Dolore, voluptatem tenetur, sequi aliquid et iure esse natus repellendus aut adipisci necessitatibus blanditiis suscipit, doloribus maxime ipsam nesciunt. Et sint exercitationem nobis officiis odio libero optio suscipit, dolore hic consequuntur excepturi tempore facilis quisquam temporibus expedita beatae nostrum non ullam dignissimos provident necessitatibus? Sequi accusantium accusamus totam modi aliquid ex expedita quasi itaque ullam laudantium, possimus quos fugiat atque animi voluptatem ipsam inventore explicabo eius sed corporis neque eos sapiente. Dolorum cum odio consequatur aliquam, a illum dolor dicta dolorem maxime autem blanditiis velit facere, consectetur dolores animi voluptatibus unde voluptatem laborum reiciendis et! Esse necessitatibus reiciendis assumenda nostrum illo enim distinctio amet quae, dicta quasi quaerat aperiam unde alias consequatur, tempora, quis labore mollitia quisquam illum corporis laborum accusantium recusandae deserunt. Culpa dolorum quas, necessitatibus nisi velit minus. Animi ducimus dolores reprehenderit autem placeat. Hic qui ipsum tempore at assumenda, maxime ad recusandae omnis sit repellendus blanditiis nulla distinctio quisquam, illo officiis. Eius illo, aliquid ut ab repudiandae molestiae quo distinctio, exercitationem asperiores molestias ipsam unde eum ducimus voluptates ad, facere voluptatum omnis quaerat dignissimos veniam obcaecati? Et, id alias. Illum maxime optio sapiente neque, ad ullam saepe veritatis doloribus ducimus voluptas velit nesciunt culpa est enim in! Sapiente, nam. Sequi unde itaque incidunt eum possimus. Quia ut porro nesciunt recusandae numquam eligendi neque cupiditate nemo vel animi, inventore suscipit sed quasi.
+					{currentInfo.event.name}?
 					</p>
 				</article>
 			</div>

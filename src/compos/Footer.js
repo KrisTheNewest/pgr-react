b8eb4271da48018c68f9d5ef1354e8888c0addab
KrisTheNewest <61835777+KrisTheNewest@users.noreply.github.com>
2024-11-04 23:02:44 +0100

    Moving the footer to a separate file

---
+++ src/compos/Footer.js
@@ -0,0 +1,17 @@
+
+function Footer() {
+	return (
+		<footer className='feet'>
+			<span style={{ fontSize: "small" }}>Made by</span>
+			<p>
+				Krystina<span style={{ fontSize: "small" }}> (website and coding)</span><br></br>
+				Senus<span style={{ fontSize: "small" }}> (data gathering)</span><br></br>
+			</p>
+			<p>
+				Powered by React™, Express™ and MongoDB™
+			</p>
+		</footer>
+	);
+}
+
+export default Footer;
--- src/compos/Layout.js
+++ src/compos/Layout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Outlet } from "react-router-dom";
 import NavBar from './NavBar';
 import SkinHistory from './SkinHistory';
+import Footer from './Footer';
 
 import '../App.css';
 
@@ -11,16 +12,7 @@ function Base() {
 			<NavBar />
 			<Outlet></Outlet>
 			<SkinHistory />
-			<footer className='feet'>
-				<span style={{ fontSize: "small" }}>Made by</span>
-				<p>
-					Krystina<span style={{ fontSize: "small" }}> (website and coding)</span><br></br>
-					Senus<span style={{ fontSize: "small" }}> (data gathering)</span><br></br>
-				</p>
-				<p>
-					Powered by React™, Express™ and MongoDB™
-				</p>
-			</footer>
+			<Footer />
 		</div>
 	)
 }
